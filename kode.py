950b11e2e713dd5da99ef46c5a02b296ffa35628
WillWG88 <70704626+WillWG88@users.noreply.github.com>
2021-01-25 19:44:25 +0700

    Update kode.py

--- kode.py
+++ kode.py
@@ -8,5 +8,5 @@ for barcode in decode(gambar):
     print(barcode.data)
     print(barcode.type)
     info=barcode.data.decode('utf-8')
-    print('data di dalam qr code ' + info)
+    print('data di dalam qr code : ' + info)
     
