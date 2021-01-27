358725a11fe6237283eed7027a98e2aba9f353a6
WillWG88 <70704626+WillWG88@users.noreply.github.com>
2021-01-27 14:02:58 +0700

    Update kode.py

--- kode.py
+++ kode.py
@@ -2,11 +2,11 @@ import cv2  # library opencv
 import numpy as np 
 from pyzbar.pyzbar import decode # library python untuk menscan QR code
 
-gambar=cv2.imread('kartustudimahasiswa.png') 
+image=cv2.imread('kartustudimahasiswa.png') 
 
-for barcode in decode(gambar):
-    print(barcode.data)
-    print(barcode.type)
-    info=barcode.data.decode('utf-8')
+for code in decode(image):
+    print(code.data)
+    print(code.type)
+    info=code.data.decode('utf-8')
     print('data di dalam qr code : ' + info)
     
